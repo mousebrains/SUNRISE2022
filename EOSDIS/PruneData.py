149b218f8fddbeba45e625900777d9381ab2cb4e
Pat Welch <pat@mousebrains.com>
2022-05-24 20:33:28 -0700

    Flatten and prune a bit better

--- EOSDIS/PruneData.py
+++ EOSDIS/PruneData.py
@@ -17,7 +17,8 @@ import logging
 
 def pruneData(info:dict, fn:str, dirname:str, force:bool=False) -> str:
     if not os.path.isfile(fn):
-        raise Exception(f"{fn} s does not exist")
+        logging.error("%s is not a file", fn)
+        return None
 
     basename = os.path.basename(fn)
     ofn = os.path.join(dirname, basename)
@@ -29,73 +30,109 @@ def pruneData(info:dict, fn:str, dirname:str, force:bool=False) -> str:
     for key in ["latMin", "latMax", "lonMin", "lonMax"]:
         if key not in info:
             logging.error("%s not in info", key)
-            sys.exit(1)
+            return None
 
     lonMin = min(info["lonMin"], info["lonMax"])
     lonMax = max(info["lonMin"], info["lonMax"])
     latMin = min(info["latMin"], info["latMax"])
     latMax = max(info["latMin"], info["latMax"])
 
-    if "geoNames" in info:
-        geoNames = info["geoNames"]
-    else:
-        geoNames = ["sst", "sst4", "chlor_a", "chl_ocx", "nflh", "sst_triple"]
+    if "geoNames" not in info:
+        logging.error("geoNames not in YAML file\n%s", json.dumps(info, indent=4, sort_keys=True))
+        return None
+
+    geoNames = info["geoNames"]
 
     with \
             xr.open_dataset(fn) as todo, \
             xr.open_dataset(fn, group="geophysical_data") as geo, \
             xr.open_dataset(fn, group="navigation_data") as nav, \
             xr.open_dataset(fn, group="scan_line_attributes") as scan:
+
+        names = {}
+        for key in geoNames:
+            if key in geo.variables:
+                names[key] = geoNames[key]
+
+        if not names:
+            logging.error("No geonames found in %s", fn)
+            return None
+
         qLatLon = np.logical_and( # points inside our lat/lon box
                 np.logical_and(nav.longitude >= lonMin, nav.longitude <= lonMax),
                 np.logical_and(nav.latitude >= latMin, nav.latitude <= latMax)).data
 
         if not qLatLon.any():
-            logging.debug("Nothing left in %s", fn)
+            logging.debug("No data in lat/lon box in %s", fn)
             return None # Nothing to prune
 
         qLine = qLatLon.any(axis=1) # There is a point in this line we are interested in
         qPixel = qLatLon.any(axis=0) # There is a point in this pixel column we are interested in
+
+        nOrig = nav.longitude.size # Original number of elements
+
         geo = geo.sel(number_of_lines=qLine, pixels_per_line=qPixel)
         nav = nav.sel(number_of_lines=qLine, pixel_control_points=qPixel)
         scan = scan.sel(number_of_lines=qLine)
         t = ((scan.year - 1970).astype("datetime64[Y]") + scan.day + scan.msec).data
+        t = np.tile(t, (nav.longitude.shape[1], 1)).T
+
+        # Now flatten and further filter on lat/lon and finite
+
+        df = xr.Dataset({
+                    "lon": ("i", nav.longitude.data.flatten()),
+                    "lat": ("i", nav.latitude.data.flatten()),
+                    "t": ("i", t.flatten()),
+                    },
+                    coords = {"i": np.arange(nav.longitude.size)},
+                )
+        
+        nLatLon = df.lon.size # Number after lat/lon box
 
         encBase = {"zlib": True, "complevel": 9}
-        enc = {"t": encBase, "lon": encBase, "lat": encBase}
+        enc = {"lon": encBase, "lat": encBase}
 
-        geoItems = {}
-        geoKeys = set(geo.keys())
-        for key in geoNames:
-            if key in geoKeys:
-                geoItems[key] = (("t", "pixel"), geo[key].data)
-                enc[key] = encBase
+        q = np.zeros(df.t.size, dtype=bool)
+        for key in sorted(names):
+            df = df.assign({key: ("i", geo[key].data.flatten())})
+            enc[key] = encBase
+            for item in names[key]:
+                if item in geo.variables:
+                    qual = geo[item].data.flatten()
+                    (llim, ulim) = names[key][item]
+                    qKeep = np.logical_and(qual >= llim, qual <= ulim)
+                    df[key][np.logical_not(qKeep)] = None
+            qKey = np.isfinite(df[key].data)
+            q = np.logical_or(q, qKey)
 
-        if not geoItems:
-            logging.error("Unsupported geo names in %s\n%s", fn, geo)
-            return None
+        df = df.sel(i=df.i[q])
 
-        ods = xr.Dataset(data_vars={
-            "lon": (("t", "pixel"), nav.longitude.data),
-            "lat": (("t", "pixel"), nav.latitude.data),
-            },
-            coords={
-                "t": t,
-                "pixel": np.arange(geo.dims["pixels_per_line"]),
-                },
-            attrs={"filename":fn},
-            )
-        ods = ods.assign(geoItems)
-        ods = ods.assign_attrs(todo.attrs)
-        ods.to_netcdf(ofn, encoding=enc)
-        szSrc = os.path.getsize(fn)  / 1024 / 1024
-        szTgt = os.path.getsize(ofn) / 1024 / 1024
-        logging.info("Pruned %s %s -> %s MB, %s%%",
-                os.path.basename(fn),
-                round(szSrc, 2),
-                round(szTgt, 2),
-                round(szTgt / szSrc * 100, 1),
+        nFinite = df.lon.size # Number ofter quality check
+
+        if not nFinite: return None # Nothing left
+
+        df = df.assign({
+            "tMin": df.t.min(),
+            "tMax": df.t.max(),
+            "tMean": df.t.mean(),
+            })
+        df = df.drop("t")
+
+        qLatLon = np.logical_and(
+                np.logical_and(df.lon >= lonMin, df.lon <= lonMax),
+                np.logical_and(df.lat >= latMin, df.lat <= latMax),
                 )
+        df.sel(i=df.i[qLatLon])
+
+        nLatLon2 = df.lon.size
+
+        logging.info("Pruned %s %s%% lat/lon %s%% finite %s%%",
+                os.path.basename(fn),
+                round(nLatLon2 / nOrig * 100, 1),
+                round(nLatLon / nOrig * 100, 1),
+                round(nFinite / nOrig * 100, 1),
+                )
+        df.to_netcdf(ofn, encoding=enc)
         return ofn
 
 

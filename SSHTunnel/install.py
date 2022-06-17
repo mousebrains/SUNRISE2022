ad15d397c776381615e61ff3c28fb718459a2dce
Pat <pat@mousebrains.com>
2022-06-17 14:03:15 +0000

    new install/uninstall

--- SSHTunnel/install.py
+++ SSHTunnel/install.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 #
-# Install the SSHtunnel service
+# Install the service(s)
 #
 # Feb-2022, Pat Welch, pat@mousebrains.com
 
@@ -13,7 +13,7 @@ def makeDirectory(dirname:str) -> str:
     dirname = os.path.abspath(os.path.expanduser(dirname))
     if not os.path.isdir(dirname):
         print("Creating", dirname)
-        os.makedirs(dirname, mode=0o755, exist_ok=True) # exist_ok=True os for race conditions
+        os.makedirs(dirname, mode=0o755, exist_ok=True) # exist_ok=True is for race conditions
     return dirname
 
 def stripComments(fn:str) -> str:
@@ -43,7 +43,6 @@ def maybeCopy(src:str, tgt:str) -> bool:
 
 parser = ArgumentParser()
 parser.add_argument("--force", action="store_true", help="Force reloading ...")
-parser.add_argument("--service", type=str, default="SSHtunnel.service", help="Service file")
 parser.add_argument("--serviceDirectory", type=str, default="~/.config/systemd/user",
         help="Where to copy service file to")
 parser.add_argument("--systemctl", type=str, default="/usr/bin/systemctl",
@@ -51,32 +50,82 @@ parser.add_argument("--systemctl", type=str, default="/usr/bin/systemctl",
 parser.add_argument("--loginctl", type=str, default="/usr/bin/loginctl",
         help="loginctl executable")
 parser.add_argument("--logdir", type=str, default="~/logs", help="Where logfiles are stored")
+parser.add_argument("services", type=str, nargs="+", help="Service file(s)")
 args = parser.parse_args()
 
-makeDirectory(args.logdir)
+timers = set()
+services = set()
+timerServices = set()
 
-root = args.serviceDirectory
-qService = maybeCopy(args.service, args.serviceDirectory)
+for item in args.services:
+    if item[-8:] == ".service":
+        services.add(item)
+    elif item[-6:] == ".timer":
+        timers.add(item)
+    else:
+        print("Unsupported file suffix in", item)
 
-if not qService and not args.force:
-    print("Nothing to be done")
+if not timers and not services:
+    print("No services specified")
+
+for item in timers: # Now split out services with timers
+    a = item.replace(".timer", ".service")
+    if a in services:
+        timerServices.add(a)
+        services.remove(a)
+
+toStart = services.union(timers) # Services and timers that need started
+onlyServices = services.union(timerServices)
+todos = list(toStart.union(timerServices)) # All services and timers 
+print("services", services)
+print("timers", timers)
+print("timerServices", timerServices)
+print("todos", todos)
+print("toStart", toStart)
+print("onlyServices", onlyServices)
+
+makeDirectory(args.logdir) # Make sure the log directory exists
+root = args.serviceDirectory # Where service files are stored
+q = False # Was a new copy of any service file installed?
+for service in todos:
+    qq = maybeCopy(service, args.serviceDirectory)
+    q |= qq
+
+if not q and not args.force:
+    print("No services were different")
     sys.exit(0)
+
+print(f"Stopping {todos}")
+cmd = [args.systemctl, "--user", "stop"]
+cmd.extend(todos)
+subprocess.run(cmd, shell=False, check=False) # Don't exit on error if services don't exist
 
 print("Forcing reload of daemon")
 subprocess.run((args.systemctl, "--user", "daemon-reload"),
         shell=False, check=True)
 
-print(f"Enabling {args.service}")
-subprocess.run((args.systemctl, "--user", "enable", args.service),
-        shell=False, check=True)
+print(f"Enabling {todos}")
+cmd = [args.systemctl, "--user", "enable"]
+cmd.extend(todos)
+subprocess.run(cmd, shell=False, check=True)
 
-print(f"Starting {args.service}")
-subprocess.run((args.systemctl, "--user", "start", args.service),
-        shell=False, check=True)
+if toStart:
+    print(f"Starting {toStart}")
+    cmd = [args.systemctl, "--user", "start"]
+    cmd.extend(toStart)
+    subprocess.run(cmd, shell=False, check=True)
 
 print("Enable lingering")
 subprocess.run((args.loginctl, "enable-linger"), shell=False, check=True)
 
-print(f"Status {args.service}")
-s = subprocess.run((args.systemctl, "--user", "--no-pager", "status", args.service),
-        shell=False, check=False)
+if onlyServices:
+    print(f"Status {onlyServices}")
+    cmd = [args.systemctl, "--user", "--no-pager", "status"]
+    cmd.extend(onlyServices)
+    s = subprocess.run(cmd, shell=False, check=False)
+
+if timers:
+    print(f"Timers {timers}")
+    cmd = [args.systemctl, "--user", "--no-pager", "list-timers"]
+    cmd.extend(timers)
+    s = subprocess.run(cmd, shell=False, check=False)

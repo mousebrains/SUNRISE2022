696a7623babddcb64012104fd978ed77874a24e4
Jamie-Hilditch <42470546+JamieHilditch@users.noreply.github.com>
2022-06-18 17:11:06 -0400

    echo error messages

--- public_html/development/php/load_hovmoller_data.php
+++ public_html/development/php/load_hovmoller_data.php
@@ -56,16 +56,19 @@ $sql.= " ORDER BY t LIMIT 10000;";
 try {
     $conn = pg_connect("dbname=$dbname");
     if (!$conn) {
+      echo(json_encode(array("error" => "unable to open database $dbname")));
 	exit(json_encode(array("error" => "unable to open database $dbname")));
     }
 
     $pe_result = pg_query_params($conn, $sql, array(implode(',',$pe_variables),'pe',$input['start_time'],$input['end_time']));
     if (!$result) {
+      echo(json_encode(array("error" => "Executing $sql")));
 	exit(json_encode(array("error" => "Executing $sql")));
     }
 
     echo(json_encode(pg_fetch_all($pe_result)));
 } catch (Exception $e) {
+  echo(json_encode(array("error" => $e->getMessage())));
 	exit(json_encode(array("error" => $e->getMessage())));
 }
 

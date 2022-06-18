1f2b5bc24585c06990cd95f43c2a1eb66e2d05d2
Jamie-Hilditch <42470546+JamieHilditch@users.noreply.github.com>
2022-06-18 17:20:34 -0400

    define pe_variables

--- public_html/development/php/load_hovmoller_data.php
+++ public_html/development/php/load_hovmoller_data.php
@@ -33,13 +33,13 @@ $variables = array(
   'V - Slab Model' => ''
 );
 
+$pe_variables = array(
+  $variables[$input['x_variable']],
+  $variables[$input['y_variable']],
+  $variables[$input['data_PE']]
+);
+
 echo(json_encode($output));
-//
-// $pe_variables = array(
-//   $variables[$input['x_variable']],
-//   $variables[$input['y_variable']],
-//   $variables[$input['data_PE']]
-// );
 //
 //
 //

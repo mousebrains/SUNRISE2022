7b889d753c02f23f08382d0f757ae035fcb7a5fc
Pat <pat@mousebrains.com>
2022-06-18 02:40:34 +0000

    Deal with restarts cleanly

--- Sync/syncadcp.pe.sh
+++ Sync/syncadcp.pe.sh
@@ -1,26 +1,31 @@
 #! /usr/bin/sh
 #
-# I tried this using 
+pattern=PE22_31_Shearman_ADCP
+
 log=/home/pat/logs/syncADCP.log
 
 date >>$log
 
-/usr/bin/rsync \
-	--archive \
-       	--verbose \
-       	--exclude=adcpdb \
-       	--exclude=config \
-       	--exclude=cal \
-       	--exclude=edit \
-       	--exclude=grid \
-       	--exclude=load \
-       	--exclude=nav \
-       	--exclude=ping \
-       	--exclude=png_archive \
-       	--exclude=quality \
-       	--exclude=scan \
-	--exclude=stick \
-	--exclude=vector \
-	/mnt/adcp/current_cruise/proc/* \
-	/mnt/sci/data/Platform/PE/ADCP_UHDAS \
-	2>&1 >>$log
+for src in /mnt/adcp/${pattern}*; do
+	tgt=/mnt/sci/data/Platform/PE/ADCP_UHDAS/`basename $src`
+	mkdir -p $tgt
+	/usr/bin/rsync \
+		--archive \
+       		--verbose \
+       		--exclude=adcpdb \
+       		--exclude=config \
+       		--exclude=cal \
+       		--exclude=edit \
+       		--exclude=grid \
+       		--exclude=load \
+       		--exclude=nav \
+       		--exclude=ping \
+       		--exclude=png_archive \
+       		--exclude=quality \
+       		--exclude=scan \
+		--exclude=stick \
+		--exclude=vector \
+		$src/proc/* \
+		$tgt \
+		2>&1 >>$log
+done

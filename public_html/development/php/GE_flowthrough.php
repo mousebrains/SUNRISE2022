d75faf5e65b961e0ad56925b5f68b1ea79b87011
Your Name <you@example.com>
2022-06-19 20:49:32 +0000

    kml works without colour

--- public_html/development/php/GE_flowthrough.php
+++ public_html/development/php/GE_flowthrough.php
@@ -9,7 +9,8 @@ header('X-Accel-Buffering: no');
 
 $nback = 48; # number of hours to search back
 
-$variables = "ROUND(lon::numeric,4),"; # longitude
+$variables = "t,"; # time
+$variables .= "ROUND(lon::numeric,4),"; # longitude
 $variables .= "ROUND(lat::numeric,4),"; # latitude
 $variables .= "ROUND(temp::numeric,4),"; # temperature
 $variables .= "ROUND(sp::numeric,4)"; # salinity
@@ -37,23 +38,150 @@ try {
 	exit(json_encode(array("error" => "Executing $sql")));
     }
 
-    echo(json_encode(pg_fetch_all($pe_result)));
-    echo(json_encode(pg_fetch_all($ps_result)));
+    $pe_data = pg_fetch_all($pe_result,PGSQL_NUM);
+    $ps_data = pg_fetch_all($ps_result,PGSQL_NUM);
 } catch (Exception $e) {
 	exit(json_encode(array("error" => $e->getMessage())));
 }
+
+// test whether we have data
+// echo(json_encode($pe_data));
+// echo("\n\n");
 
 // spit out KML via the XMLWriter
 
 $r = new XMLWriter();
 $r->openMemory(); // Build in memory
+$r->setIndent(true);
 $r->startDocument("1.0", "UTF-8"); // XML type
 $r->startElement("kml"); // Start a kml stanza
 $r->writeAttribute("xmlns", "http://www.opengis.net/kml/2.2");
 $r->startElement("Document");
 $r->writeElement("name", "Flowthrough");
 
-//$r->startElement("") //
+$r->startElement("Style");
+$r->writeAttribute("id","sunrise_lines");
+$r->startElement("LineStyle");
+$r->writeElement("width",5);
+$r->endElement(); // Linestyle
+$r->startElement("BalloonStyle");
+$r->startElement("text");
+$text_string = "$[sunriseData/Time/displayName] $[sunriseData/Time]<br/>";
+$text_string .= "$[sunriseData/lon/displayName] $[sunriseData/lon]<br/>";
+$text_string .= "$[sunriseData/lat/displayName] $[sunriseData/lat]<br/>";
+$text_string .= "$[sunriseData/temp/displayName] $[sunriseData/temp]<br/>";
+$text_string .= "$[sunriseData/sal/displayName] $[sunriseData/sal]";
+$r->writeCData($text_string);
+$r->endElement(); // text
+$r->endElement(); // BalloonStyle
+$r->endElement(); // Style
+
+$r->startElement("Schema");
+$r->writeAttribute("name","sunriseData");
+$r->writeAttribute("id","sunriseData");
+$r->startElement("SimpleField");
+$r->writeAttribute("type","string");
+$r->writeAttribute("name","Time");
+$r->startElement("displayName");
+$r->writeCData("<b>Time</b>");
+$r->endElement(); // displayName;
+$r->endElement(); // SimpleField - time
+$r->startElement("SimpleField");
+$r->writeAttribute("type","float");
+$r->writeAttribute("name","lon");
+$r->startElement("displayName");
+$r->writeCData("<b>Longitude</b>");
+$r->endElement();
+$r->endElement(); // SimpleField - lon
+$r->startElement("SimpleField");
+$r->writeAttribute("type","float");
+$r->writeAttribute("name","lat");
+$r->startElement("displayName");
+$r->writeCData("<b>Latitude</b>");
+$r->endElement();
+$r->endElement(); // SimpleField - lat
+$r->startElement("SimpleField");
+$r->writeAttribute("type","float");
+$r->writeAttribute("name","temp");
+$r->startElement("displayName");
+$r->writeCData("<b>Temperature</b>");
+$r->endElement();
+$r->endElement(); // SimpleField - temp
+$r->startElement("SimpleField");
+$r->writeAttribute("type","float");
+$r->writeAttribute("name","sal");
+$r->startElement("displayName");
+$r->writeCData("<b>Salinity</b>");
+$r->endElement();
+$r->endElement(); // SimpleField - sal
+$r->endElement(); // Schema
+
+$r->startElement("Folder"); // Pelican salinity folder
+$r->writeAttribute("id","PE_salinity");
+$r->writeElement("name","PE Salinity");
+
+$start_time = $pe_data[0][0];
+$start_lon = $pe_data[0][1];
+$start_lat = $pe_data[0][2];
+foreach (range(1, count($pe_data)-1) as $ii) {
+	$end_time = $pe_data[$ii][0];
+	$end_lon = $pe_data[$ii][1];
+	$end_lat = $pe_data[$ii][2];
+	$end_temp = $pe_data[$ii][3];
+	$end_sal = $pe_data[$ii][4];
+	
+	$r->startElement("Placemark");
+
+	$r->startElement("TimeSpan");
+	$r->writeElement("begin",str_replace(' ','T',$start_time).":00");
+	$r->writeElement("end",str_replace(' ','T',$end_time).":00");
+	$r->endElement(); // TimeSpan
+	$r->startElement("LineString");
+	$r->writeElement("coordinates","$start_lon,$start_lat $end_lon,$end_lat");
+	$r->endElement(); // Linestring
+
+
+	$r->writeElement("styleUrl","#sunrise_lines");
+	$r->startElement("Style");
+	$r->startElement("LineStyle");
+	$r->writeElement("color","ff000000");
+	$r->endElement(); // LineStyle
+	$r->endElement(); // Style
+	
+	$r->startElement("ExtendedData");
+	$r->startElement("SchemaData");
+	$r->writeAttribute("schemaUrl","#sunriseData");
+	$r->startElement("SimpleData");
+	$r->writeAttribute("name","Time");
+	$r->text(substr($end_time,0,16));
+	$r->endElement(); // SimpleData - Time
+	$r->startElement("SimpleData");
+	$r->writeAttribute("name","lon");
+	$r->text($end_lon);
+	$r->endElement(); // SimpleData - lon
+	$r->startElement("SimpleData");
+	$r->writeAttribute("name","lat");
+	$r->text($end_lat);
+	$r->endElement(); // SimpleData - lat
+	$r->startElement("SimpleData");
+	$r->writeAttribute("name","temp");
+	$r->text($end_temp);
+	$r->endElement(); // SimpleData - temp
+	$r->startElement("SimpleData");
+	$r->writeAttribute("name","sal");
+	$r->text($end_sal);
+	$r->endElement(); // SimpleData - sal
+	$r->endElement(); // SchemaData 
+	$r->endElement(); // ExtendedData
+	
+	$r->endElement(); // Placemark
+
+	$start_time = $end_time;
+	$start_lon = $end_lon;
+	$start_lat = $end_lat;	
+}
+
+$r->endElement(); // Pelican salinity folder
 
 $r->endElement(); // Document
 $r->endElement(); // kml

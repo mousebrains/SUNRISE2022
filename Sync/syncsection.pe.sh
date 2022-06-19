a6abe6a0a4bbc918d891ff278e9dc6cfd99788d8
Pat <pat@mousebrains.com>
2022-06-19 21:45:40 +0000

    Initial cut on syncing the section files

---
+++ Sync/syncsection.pe.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/sh
+#
+log=/home/pat/logs/syncSection.log
+
+src0=/mnt/sci/code/section/PE_section_definition.csv
+tgt0=/home/pat/Sync/Pelican/section
+
+src1=/home/pat/Sync/PointSur/section/PS_section_definition.csv
+tgt1=/mnt/sci/code/section
+
+mkdir -p $tgt0
+
+date >>$log
+
+/usr/bin/rsync --archive --verbose $src0 $tgt0 2>&1 >>$log
+/usr/bin/rsync --archive --verbose $src1 $tgt1 2>&1 >>$log

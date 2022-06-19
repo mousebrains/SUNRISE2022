c9cd27dab2d74b69a5ff8ee9860d0cf966fa9bab
Your Name <you@example.com>
2022-06-19 01:34:38 +0000

    Calculating inertial periods

--- public_html/development/php/ft_db.php
+++ public_html/development/php/ft_db.php
@@ -4,9 +4,15 @@ header('Content-Type: application/json');
 header('Cache-Control: no-cache');
 header('X-Accel-Buffering: no');
 
-$nback = array_key_exists("nback", $_GET) ? $_GET["nback"] : 30; # Number of minutes into the past
+$nback = array_key_exists("nback", $_GET) ? $_GET["nback"] : 120; # Number of minutes into the past
 $ship = array_key_exists("ship", $_GET) ? $_GET["ship"] : "pe"; # Name of ship
 $names = array_key_exists("vars", $_GET) ? $_GET["vars"] : "temp"; # Variables to fetch
+
+$names = "temp,";
+$names .= "((DATE_PART('day', t::timestamp - '2022-06-17 12:00:00'::timestamp) * 24";
+$names .= " + DATE_PART('hour', t::timestamp - '2022-06-17 12:00:00'::timestamp)) * 60";
+$names .= " + DATE_PART('minute', t::timestamp - '2022-06-17 12:00:00'::timestamp))/1497,sp"; # Trying to get temp,inertial periods,salinity
+
 $dbname = "sunrise";
 
 $sql = "SELECT t,$names FROM met";
@@ -25,7 +31,7 @@ try {
 	exit(json_encode(array("error" => "Executing $sql")));
     }
 
-    echo(json_encode(pg_fetch_all($result)));
+    echo(json_encode(pg_fetch_all($result,PGSQL_NUM)));
 } catch (Exception $e) {
 	exit(json_encode(array("error" => $e->getMessage())));
 }

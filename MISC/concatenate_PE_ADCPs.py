89272a3796428840330d4a38cc4b4126173e6342
Jamie-Hilditch <42470546+JamieHilditch@users.noreply.github.com>
2022-06-21 20:59:22 -0500

    use xarray to create concatenated adcp

--- MISC/concatenate_PE_ADCPs.py
+++ MISC/concatenate_PE_ADCPs.py
@@ -1,36 +1,36 @@
 import netCDF4
 import os
-import shutil
+import xarray as xr
 
 def concatenate_adcp(adcp: str) -> None:
     source = f"/mnt/adcp/PE22_31_Shearman_ADCP_*/proc/{adcp}/contour/{adcp}.nc"
     target = f"/mnt/sci/data/Processed_NC/ADCP_UHDAS/{adcp}_concat.nc"
 
-
     # create the target file if it does not exist
     if not os.path.isfile(target):
-        
+
         # use the first of the source files as a template
+        # however we need to ensure the time dimension is unlimited
+        # therefore use xarray to do the copy
         template = source.replace("*","1",1)
         if not os.path.isfile(template):
             raise FileNotFoundError(f"Source file '{template}' does not exist")
-        print(f"Creating file {target}")
-        shutil.copy(template, target)
+        tmp = xr.open_dataset(template)
+        tmp.to_netcdf(target,unlimited_dims=("time"),format="NETCDF3_CLASSIC")
+        print(f"Created file {target}")
 
     # now open the target file and the source files as a multifile dataset
     with netCDF4.MFDataset(source,aggdim='time') as src, \
         netCDF4.Dataset(target,'a') as tgt:
 
-        # ensure that the time dimenion is appendable
-        if not 
 
         # these datasets have no groups so we can simply loop over the variables
         for key in tgt.variables.keys():
             tgt_shape = tgt[key].shape
             src_shape = src[key].shape
-            
+
             # skip scalar variables
-            if not tgt_shape: 
+            if not tgt_shape:
                 continue
             else:
                 tgt_idx = tgt_shape[0]

6b1c3fef3780e74788eb0426620b2bce094f46e0
Pat <pat@mousebrains.com>
2022-06-23 16:38:33 +0000

    Exclude Cal files

--- MISC/adcpJoin.py
+++ MISC/adcpJoin.py
@@ -10,25 +10,56 @@ import xarray as xr
 import netCDF4
 import glob
 import os
+import re
 import sys
 
-def concatenate_adcp(adcp:str, files: list, args:ArgumentParser) -> None:
-    source = os.path.join(args.adcp, args.pattern + "*", "proc", adcp, "contour", adcp + ".nc")
+def findFiles(args:ArgumentParser) -> dict:
+    pattern = os.path.abspath(os.path.expanduser(os.path.join(
+        args.adcp, args.pattern + "*" ,"proc", "*", "contour", "*.nc")))
+    files = {}
+    toExclude = None
+    if args.exclude:
+        toExclude = []
+        for item in args.exclude:
+            toExclude.append(re.compile(item))
+    
+    for fn in glob.glob(pattern):
+        if toExclude:
+            qSkip = False
+            for item in toExclude:
+                if item.search(fn):
+                    logging.debug("Skipping %s %s", fn, item)
+                    qSkip = True
+                    break
+            if qSkip: continue
+
+        mtime = os.path.getmtime(fn)
+        adcp = os.path.basename(fn)[:-3]
+        if adcp not in files: files[adcp] = {}
+        if mtime not in files[adcp]: files[adcp][mtime] = []
+        files[adcp][mtime].append(fn)
+
+    adcps = {}
+    for adcp in sorted(files):
+        adcps[adcp] = []
+        for mtime in sorted(files[adcp]):
+            adcps[adcp].extend(files[adcp][mtime])
+
+    return adcps
+
+def concatenate_adcp(adcp:str, source: list[str], args:ArgumentParser) -> None:
     target = os.path.join(args.processed,
             args.cruise + "_" + args.platform + "_" + adcp + "_concat.nc")
 
     logging.debug("ADCP %s source %s tgt %s", adcp, source, target)
 
-    # create the target file if it does not exist
-    if not os.path.isfile(target):
+    if not os.path.isfile(target): # create the target file if it does not exist
         # use the first of the source files as a template
         # however we need to ensure the time dimension is unlimited
         # therefore use xarray to do the copy
-        files = sorted(files) # Earliest first
-        template = files[0]
-        tmp = xr.open_dataset(template)
+        tmp = xr.open_dataset(source[0]) # Source is sorted based on mtime
         tmp.to_netcdf(target,unlimited_dims=("time"),format="NETCDF3_CLASSIC")
-        logging.info("Created %s from %s", target, template)
+        logging.info("Created %s from %s", target, source[0])
 
     # now open the target file and the source files as a multifile dataset
     with netCDF4.MFDataset(source, aggdim='time') as src, \
@@ -36,29 +67,29 @@ def concatenate_adcp(adcp:str, files: list, args:ArgumentParser) -> None:
 
         tgt_idx = len(tgt.dimensions["time"])
         src_idx = len(src.dimensions["time"])
-        logging.info("%s target length %s source length %s adding %s datapoints",
-                adcp, tgt_idx, src_idx, src_idx-tgt_idx)
 
         # check there is new data to add
         if tgt_idx >= src_idx:
             logging.info("No new data for %s", adcp)
             return
 
+        logging.info("%s target length %s source length %s adding %s datapoints",
+                adcp, tgt_idx, src_idx, src_idx-tgt_idx)
+
         # these datasets have no groups so we can simply loop over the variables
         for key in tgt.variables.keys():
             tgt_shape = tgt[key].shape
-            src_shape = src[key].shape
 
             # skip scalar variables
             if not tgt_shape: continue
 
             # append new data
-            if len(tgt_shape) == 1:
+            if len(tgt_shape) == 1: # Vector
                 tgt[key][tgt_idx:src_idx] = src[key][tgt_idx:src_idx]
-            elif len(tgt_shape) == 2:
+            elif len(tgt_shape) == 2: # Array
                 tgt[key][tgt_idx:src_idx,:] = src[key][tgt_idx:src_idx,:]
             else:
-                raise ValueError(f"ADCP, {adcp} {key}, variables have either 1 or 2 dimensions")
+                raise ValueError(f"ADCP shape issue, {adcp} {key} shape %s{tgt_shape}")
 
 
 if __name__ == "__main__":
@@ -67,6 +98,8 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     Logger.addArgs(parser)
     parser.add_argument("--pattern", type=str, required=True, help="UHDAS pattern to concatenate")
+    parser.add_argument("--exclude", type=str, action="append",
+            help="Regular expressions in patterns to exclude")
     parser.add_argument("--platform", type=str, help="2 letter platform prefix")
     parser.add_argument("--adcp", type=str, default="/mnt/adcp", help="UHDAS directory")
     parser.add_argument("--processed", type=str, default="/mnt/sci/data/Processed_NC/ADCP_UHDAS",
@@ -78,15 +111,10 @@ if __name__ == "__main__":
 
     if args.platform is None: args.platform = args.pattern[:2]
 
-    files = {}
-    for fn in glob.glob(os.path.join(args.adcp, args.pattern + "*", "proc", "*", "contour", "*.nc")):
-        adcp = os.path.basename(fn)[:-3]
-        if adcp not in files: files[adcp] = []
-        files[adcp].append(fn)
-
-    logging.info("ADCPS %s", files)
-
     logging.info("Args %s", args)
+
+    files = findFiles(args)
+
     for adcp in files:
         try:
             concatenate_adcp(adcp, files[adcp], args)

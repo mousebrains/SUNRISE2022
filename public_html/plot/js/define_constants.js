91ab91adf7f70e1534de4c95287b1849ae0b3cdd
Jamie-Hilditch <42470546+JamieHilditch@users.noreply.github.com>
2022-06-23 22:00:16 -0500

    added chlorophyll units

--- public_html/plot/js/define_constants.js
+++ public_html/plot/js/define_constants.js
@@ -42,11 +42,14 @@ const LABELS = {
   "Longitude": "Longitude [ \u00B0E ]",
   "Salinity": "Salinity [ PSU ]",
   "Temperature": "Temperature [ \u00B0C ]",
+  "Chlorophyll": "Chlorophyll [ \u03BCg/L ]",
   "U - Slab Model": "U [ m<sup>2</sup>/s ]",
   "V - Slab Model": "V [ m<sup>2</sup>/s ]",
   "Air Temperature": "Air Temperature [ \u00B0C ]",
 	"Air Pressure": "Air Pressure [ mbar ]",
 	"Relative Humidity": "Relative Humidity [ % ]",
 	"Wind Speed": "Wind Speed [ m/s ]" ,
-	"Wind Direction": "Wind Direction [ \u00B0 ]"
+	"Wind Direction": "Wind Direction [ \u00B0 ]",
+  "Wind u": "Wind u [ m/s ]",
+  "Wind v": "Wind v [ m/s ]"
 }
